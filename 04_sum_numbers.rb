4f27628698b8806af49ae07972a0c0d77b16c2cb
RyanG91 <ryan.grosser91@gmail.com>
2019-02-06 14:15:00 +1000

    Sum numbers challenge added

---
+++ 04_sum_numbers.rb
@@ -0,0 +1,15 @@
+# Sum Numbers
+
+# Write a method that will take an array of numbers,
+# and return their sum.
+
+# Example:
+# sum_numbers([1,1,1]) should return 3
+# sum_numbers([5,2,100,0,10]) should return 117
+
+# Check your solution by running the tests:
+# ruby tests/04_sum_numbers_test.rb
+
+def sum_numbers (numbers)
+  # Your code here
+end
